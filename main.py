2916e62151a998d959ab4b477975e5b0b8a035d0
Kevin <pintokevin2002@hotmail.com>
2025-04-25 16:05:59 +0200

    Adding types

--- main.py
+++ main.py
@@ -8,33 +8,34 @@ from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
 # Getting the guild ID from the environment variable
-GUILD_ID = discord.Object(id=os.getenv("GUILD_ID")) if os.getenv("PYTHON_ENV") == "dev" else None
+GUILD_ID: discord.Object|None = discord.Object(id=os.getenv("GUILD_ID")) if os.getenv("PYTHON_ENV") == "dev" else None
 
 class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
         try:
-            synced = await self.tree.sync(guild=GUILD_ID)
-            display_id = GUILD_ID.id if GUILD_ID else "global"
+            synced: list[discord.app_commands.AppCommand] = await self.tree.sync(guild=GUILD_ID)
+            display_id: int|str = GUILD_ID.id if GUILD_ID else "global"
             print(f'Synced {len(synced)} commands to guild {display_id}')
 
         except Exception as e:
             print(f'Error syncing commands: {e}')
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         if message.author == self.user:
             return
 
         if message.content.startswith('hello'):
             await message.channel.send(f'Hi there {message.author} : {message.author.mention}')
 
-    async def on_reaction_add(self, reaction, user):
+    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
         await reaction.message.channel.send(f'{user} reacted using {reaction.emoji}')
 
+# Define the intents and create the client
 intents = discord.Intents.default()
 intents.message_content = True
-client = Client(command_prefix="!", intents=intents)
+client: Client = Client(command_prefix="!", intents=intents)
 
 @client.tree.command(name="hello", description="Say hello", guild=GUILD_ID)
 async def say_hello(interaction: discord.Interaction):
@@ -46,7 +47,7 @@ async def printer(interaction: discord.Interaction, printer: str):
 
 @client.tree.command(name="embed", description="Embed demo", guild=GUILD_ID)
 async def embed_demo(interaction: discord.Interaction):
-    embed = discord.Embed(title="I am a title", url="https://www.crunchyroll.com/fr/series/GQWH0M1J3/to-be-hero-x", description="I am a description", color=discord.Color.red())
+    embed: discord.Embed = discord.Embed(title="I am a title", url="https://www.crunchyroll.com/fr/series/GQWH0M1J3/to-be-hero-x", description="I am a description", color=discord.Color.red())
     embed.set_thumbnail(url="https://placebear.com/200/200")
     embed.add_field(name="Field 1 title", value="Here comes something", inline=False)
     embed.add_field(name="Field 2 title", value="Value 2", inline=True)
@@ -57,15 +58,15 @@ async def embed_demo(interaction: discord.Interaction):
 
 class View(discord.ui.View):
     @discord.ui.button(label="Click me!", style=discord.ButtonStyle.blurple, emoji="🙏")
-    async def button_callback_1(self, button, interaction):
+    async def button_callback_1(self, button: discord.ui.Button, interaction: discord.Interaction):
         await button.response.send_message("You clicked me!")
 
     @discord.ui.button(label="I'm a button", style=discord.ButtonStyle.red, emoji="🔥")
-    async def button_callback_2(self, button, interaction):
+    async def button_callback_2(self, button: discord.ui.Button, interaction: discord.Interaction):
         await button.response.send_message("You burn your finger")
 
     @discord.ui.button(label="Don't click me", style=discord.ButtonStyle.secondary)
-    async def button_callback_3(self, button, interaction):
+    async def button_callback_3(self, button: discord.ui.Button, interaction: discord.Interaction):
         await button.response.send_message("Why did you do that")
 
 @client.tree.command(name="buttons", description="Displaying some buttons", guild=GUILD_ID)
